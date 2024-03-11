b1cf7d410c47559a9134aa7765cd73d4367eaf23
Meghan Leicht <mleicht92@gmail.com>
2024-03-10 20:09:24 -0700

    trying to deploy by changing to hashrouter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
 
 reportWebVitals();
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
+
+// reportWebVitals();
