4eb154f32691dad4b397b31ede70360bfb2fba77
Meghan Leicht <mleicht92@gmail.com>
2024-03-12 11:50:02 -0700

    added functionality to close the menu when navigated to a link

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -23,10 +23,18 @@ function Navbar() {
         </button>
       </div>
       <div className="hidden-links" id={openMenu ? "open" : "close"}>
-        <a href="#home">Home</a>
-        <a href="#about">About Me</a>
-        <a href="#projects">Projects</a>
-        <a href="#contact">Contact</a>
+        <a href="#home" onClick={toggleMenu}>
+          Home
+        </a>
+        <a href="#about" onClick={toggleMenu}>
+          About Me
+        </a>
+        <a href="#projects" onClick={toggleMenu}>
+          Projects
+        </a>
+        <a href="#contact" onClick={toggleMenu}>
+          Contact
+        </a>
       </div>
     </nav>
   );
