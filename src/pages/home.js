038071ce91362c2a1360e96196d2fc848187062c
FemputerCodes <mleicht92@gmail.com>
2024-03-13 16:54:25 -0700

    again, trying to smooth the harsh line between cave and main. it can only be seen on my iphone and ipad.

--- src/pages/home.js
+++ src/pages/home.js
@@ -13,12 +13,8 @@ function Home() {
     const scrollPosition = window.scrollY;
     setScrollPosition(scrollPosition);
     // console.log("scroll position: ", scrollPosition);
-    // if (window.innerWidth > 768) {
     refSky.current.style.left = `${-scrollPosition * 0.08}px`;
     refTitle.current.style.marginTop = `${scrollPosition * 0.8}px`;
-    // } else {
-    // refTitle.current.style.marginTop = `${scrollPosition * 1.1}px`;
-    // }
   };
 
   useEffect(() => {
